29b6defb53687c3cba54ddf7d01cd1cc56654191
Dhanraj Jamdade <dhanraj.jamdade@gmail.com>
2020-10-03 15:45:55 +0530

    Update blockjump.py

--- blockjump.py
+++ blockjump.py
@@ -6,26 +6,20 @@ screenSize = 600
 pygame.display.set_caption('BlockJump')
 win = pygame.display.set_mode((screenSize,screenSize))
 score = 0
-clock = pygame.time.Clock()
-global run
 class player(object):
     def __init__(self,x,y,width,height):
         self.x = x
         self.y = y 
         self.width = width 
         self.height = height
-        self.vel = 10
+        self.vel = 11
         self.jumpCount = 7
         self.isJump = False
-        self.hitbox = (self.x , self.y  , 40, 40)
-        self.health = 100
+        self.hitbox = (self.x, self.y, self.width, self.height)
         
     def draw(self,win):
-        self.hitbox = (self.x , self.y  , 40, 40)
-        pygame.draw.rect(win, (255,0,0), (self.x,self.y,self.width,self.height))
-        
-    def hit(self):
-        pass
+        self.hitbox = (self.x, self.y, self.width, self.height)
+        pygame.draw.rect(win, (255,0,0), (int(self.x), int(self.y), int(self.width), int(self.height)))
         
 class enemy(object):
     def __init__ (self,x,y,height,width):
@@ -34,20 +28,19 @@ class enemy(object):
         self.height = height
         self.width = width
         self.vel = 9
-        self.hitbox = (self.x , self.y  , 40, 40)
-        self.health = 10
+        self.hitbox = (self.x, self.y, self.width, self.height)
         self.visible = True
     
     def draw(self,win):
         self.move() 
         if self.visible :
-            self.hitbox = (self.x , self.y  , 40, 40)
-            pygame.draw.rect(win, (255,255,0), (self.x,self.y,self.width,self.height))
+            self.hitbox = (self.x, self.y, self.width, self.height)
+            pygame.draw.rect(win, (255,255,0), (int(self.x), int(self.y), int(self.width), int(self.height)))
 
     def move(self):
-        if self.vel>0 :
+        if self.vel > 0 :
             self.vel += 0.03
-            if(self.x + self.vel < screenSize-40):
+            if(self.x + self.vel < screenSize - 40):
                 self.x += self.vel
             else:
                 self.vel *= -1
@@ -57,26 +50,20 @@ class enemy(object):
             else:
                 self.vel *= -1
     
-    def hit(self):
-        if self.health>0 :
-            self.health -= 1
-        if self.health==0 :
-            self.visible = False
-    
 class goal(object):
     def __init__ (self,x,y,height,width):
         self.x = x
         self.y = y
         self.height = height
         self.width = width
-        self.hitbox = (self.x , self.y  , 10, 10)
+        self.hitbox = (self.x, self.y, self.width, self.height)
         self.is_hit = False
     
     def draw(self,win):
         if self.is_hit :
             self.move()
-        self.hitbox = (self.x , self.y  , 10, 10)
-        pygame.draw.rect(win, (255,255,255), (self.x,self.y,self.width,self.height))
+        self.hitbox = (self.x, self.y, self.width, self.height)
+        pygame.draw.rect(win, (255,255,255), (int(self.x), int(self.y), int(self.width), int(self.height)))
 
     def move(self):
         self.x = random.randint(0,590)
@@ -87,18 +74,18 @@ class goal(object):
         
 def redrawWindow():
     win.fill((0,0,0))
-    font = pygame.font.SysFont('verdana', 18, False, True)
-    quit_inst = font.render('Press q to quit', True, (255, 255, 255))
-    win.blit(quit_inst, (10,10))
+    font = pygame.font.SysFont('verdana', 15, False, True)
+    quit_inst = font.render('Press q to quit', True, (155, 155, 155))
+    win.blit(quit_inst, (10, 10))
     
     font = pygame.font.SysFont('verdana', 22)
     score_text = font.render('score : ' + str(score), True, (105,105,105))
-    win.blit(score_text, (300 - score_text.get_width()/2,300))
+    win.blit(score_text, (int(screenSize/2 - score_text.get_width()/2), int(300)))
     
-    pygame.draw.rect(win, (255,255,255), (0,343,600,20))
+    pygame.draw.rect(win, (255,255,255), (0,343,screenSize,10))
     font = pygame.font.SysFont('verdana', 12)
     text = font.render('dhanraj322.github.io', True, (255, 255, 255))
-    win.blit(text, (580 - text.get_width(),570))
+    win.blit(text, (screenSize - 20 - text.get_width(), screenSize - 30))
     goal1.draw(win)
     obs.draw(win)
     man.draw(win)
@@ -110,11 +97,10 @@ def start_screen():
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
                  intro = False
-        pygame.time.delay(40)
         win.fill((0,0,0))
         font = pygame.font.SysFont('verdana', 40, True)
         text = font.render('START', True, (255, 255, 255))
-        win.blit(text, (300 - text.get_width()/2,50))
+        win.blit(text, (int(300 - text.get_width()/2),int(50)))
         font = pygame.font.SysFont('verdana', 18)
         
         text = font.render('Move', True, (255, 255, 255))
@@ -123,10 +109,10 @@ def start_screen():
         
         font = pygame.font.SysFont('verdana', 17, False, True)
         text = font.render('a to left        d to right', True, (255, 255, 255))
-        win.blit(text, (300 - text.get_width()/2,200))
+        win.blit(text, (int(300 - text.get_width()/2),int(200)))
         
         text = font.render('space to jump', True, (255, 255, 255))
-        win.blit(text, (300 - text.get_width()/2,230))
+        win.blit(text, (int(300 - text.get_width()/2),int(230)))
         
         font = pygame.font.SysFont('verdana', 17)
         text = font.render('Goal is to collect', True, (255, 255, 255))
@@ -139,14 +125,14 @@ def start_screen():
         
         font = pygame.font.SysFont('verdana', 17, False, True)
         text = font.render('Press w to start', True, (255, 255, 255))
-        win.blit(text, (300 - text.get_width()/2,500))
+        win.blit(text, (300 - text.get_width()//2,500))
         
         text = font.render('Press q to quit', True, (255, 255, 255))
-        win.blit(text, (300 - text.get_width()/2,530))
+        win.blit(text, (300 - text.get_width()//2,530))
             
         font = pygame.font.SysFont('verdana', 12)
         text = font.render('dhanraj322.github.io', True, (255, 255, 255))
-        win.blit(text, (580 - text.get_width(),570))
+        win.blit(text, (screenSize - 20 - text.get_width(),screenSize - 30))
         pygame.display.update()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
@@ -156,19 +142,18 @@ def start_screen():
             sys.exit(0)
             
 def exit_screen():
-    pygame.time.delay(40)
     win.fill((0,0,0))
     font = pygame.font.SysFont('verdana', 35, True, True)
     text = font.render('Game Over', True, (255, 255, 255))
-    win.blit(text, (300 - text.get_width()/2,200))
+    win.blit(text, (300 - text.get_width()//2,200))
     font = pygame.font.SysFont('verdana', 18)
     text = font.render('Score : ' + str(score), True, (255, 255, 255))
-    win.blit(text, (300 - text.get_width()/2,300))
+    win.blit(text, (300 - text.get_width()//2,300))
     font = pygame.font.SysFont('verdana', 12)
     text = font.render('dhanraj322.github.io', True, (255, 255, 255))
     win.blit(text, (580 - text.get_width(),570))
     pygame.display.update()
-    pygame.time.delay(2000)
+    pygame.time.delay(1000)
     
 man = player(20,300,40,40)
 obs = enemy(500,300,40,40)
@@ -177,12 +162,11 @@ start_screen()
 run = True
 
 while run:
-    pygame.time.delay(40)
+    pygame.time.delay(30)
         
-    if man.hitbox[0] <= obs.hitbox[0] + obs.hitbox[2] and man.hitbox[0] >= obs.hitbox[0] :
-        if man.hitbox[1] <= obs.hitbox[1] + obs.hitbox[3] and man.hitbox[1] >= obs.hitbox[1]:
-            man.hit()
-            pygame.time.delay(500)
+    if (man.hitbox[0] <= obs.hitbox[0] and man.hitbox[0] + man.hitbox[2] >= obs.hitbox[0]) or (man.hitbox[0] <= obs.hitbox[2] + obs.hitbox[0] and obs.hitbox[0] + obs.hitbox[2] <= man.hitbox[0] + man.hitbox[2]):
+        if (man.hitbox[1] <= obs.hitbox[1] + obs.hitbox[3] and man.hitbox[1] >= obs.hitbox[1]) or (man.hitbox[1] + man.hitbox[3] <= obs.hitbox[1] + obs.hitbox[3] and man.hitbox[1] + man.hitbox[3] >= obs.hitbox[1]) :
+            pygame.time.delay(600)
             exit_screen()
             man = player(20,300,40,40)
             obs = enemy(500,300,40,40)
@@ -190,9 +174,9 @@ while run:
             start_screen()
             run = True
             score = 0
-            
-    if goal1.hitbox[0] <= man.hitbox[0] + man.hitbox[2] and goal1.hitbox[0] >= man.hitbox[0] :
-        if goal1.hitbox[1] <= man.hitbox[1] + man.hitbox[3] and goal1.hitbox[1] >= man.hitbox[1]:
+    
+    if (man.hitbox[0] <= goal1.hitbox[0] and man.hitbox[0] + man.hitbox[2] >= goal1.hitbox[0]) or (man.hitbox[0] <= goal1.hitbox[2] + goal1.hitbox[0] and goal1.hitbox[0] + goal1.hitbox[2] <= man.hitbox[0] + man.hitbox[2]):
+        if (man.hitbox[1] <= goal1.hitbox[1] + goal1.hitbox[3] and man.hitbox[1] >= goal1.hitbox[1]) or (man.hitbox[1] + man.hitbox[3] <= goal1.hitbox[1] + goal1.hitbox[3] and man.hitbox[1] + man.hitbox[3] >= goal1.hitbox[1]) :  
             goal1.hit()
             score+=1
     
